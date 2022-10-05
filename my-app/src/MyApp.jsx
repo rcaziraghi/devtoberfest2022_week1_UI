93c66809255b5d2c4a939f51ce0fd7ae9a11037e
rcaziraghi <rcaziraghi@gmail.com>
2022-10-05 09:44:00 +0100

    Card exercise done.

--- my-app/src/MyApp.jsx
+++ my-app/src/MyApp.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { Button, Card, CardHeader, Text } from '@ui5/webcomponents-react';
+import { spacing } from "@ui5/webcomponents-react-base";
 
 export function MyApp() {
-  return <div>My root component</div>;
+  const handleHeaderClick = () => {
+    alert("Header clicked");
+  };
+  return <div>
+          <Card header={
+                <CardHeader 
+                  titleText="Card" 
+                  interactive 
+                  onClick={handleHeaderClick} />
+                  } 
+                style={{ width: "300px" }}>
+            <Text style={spacing.sapUiContentPadding}>
+              This is the content area of the Card
+            </Text>
+            <Button onClick={() => alert('Hello World!')}>Hello world!</Button>
+          </Card>
+        </div>;
 }
